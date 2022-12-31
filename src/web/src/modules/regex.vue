f8c64efc7025cfb02462b64041d53cc936b593d8
毛线球上的喵星人 <785315332@qq.com>
2022-12-31 13:07:57 +0800

    完善路由

---
+++ src/web/src/modules/regex.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    regex
+  </div>
+</template>
--- src/web/src/router/index.js
+++ src/web/src/router/index.js
@@ -1,9 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import moduleList from '@/moduleList.json'
+
+const createModuleRoutes = _moduleList => {
+  const results = []
+  const addToRoutes = (module, routes) => {
+    const isLeaf = !module.children || module.type == 'module'
+    let item = {
+      path: module.code,
+      name: module.name,
+      children: isLeaf ? null : []
+    }
+    if (isLeaf) {
+      item.component = () => import(`@/modules/${module.code}.vue`)
+    }
+    routes.push(item)
+    return item
+  }
+  const travelEach = (list, resList) => {
+    if (!list) { return }
+    list.map(item => {
+      let route = addToRoutes(item, resList)
+      travelEach(item.children, route.children)
+    })
+  }
+  travelEach(_moduleList, results)
+
+  return results
+}
+
+const moduleRoutes = createModuleRoutes(moduleList)
+console.log(moduleRoutes)
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/tool',
+      children: moduleRoutes
+    }
   ]
+})
+
+router.beforeEach((to, from) => {
+  console.log(to, from)
+  document.title = to.name ? `${to.name} | MxTools` : 'MxTools'
+  return true;
 })
 
 export default router
