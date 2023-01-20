6aae573fe789ffab6be97ccd93c0af395feaf49e
毛线球上的喵星人 <785315332@qq.com>
2023-01-20 19:44:31 +0800

    优化模块，删除无用文件，将模块统一到homeview中处理布局

--- src/web/src/views/HomeView/components/Module.vue
+++ src/web/src/views/HomeView/components/Module.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { computed } from 'vue';
-import Module from './Module.vue'
-import ModuleItem from './ModuleItem.vue'
+import { useRouter } from 'vue-router';
 import { useModuleRouteStore } from '@/stores/moduleRoutes';
 import { storeToRefs } from 'pinia';
+import { calIsShow } from '@/modules/tools/moduleRouteUtils';
 
 const props = defineProps({
   module: {
@@ -14,35 +14,56 @@ const props = defineProps({
   }
 })
 
-const moduleRouteStore = useModuleRouteStore()
-const { filterPattern } = storeToRefs(moduleRouteStore)
-const isShow = computed(() => module => {
-  console.log(module)
-  return (module.path.indexOf(filterPattern.value) !== -1) ? 1 : 0
-})
-
-const respondCols = () => {
-  let str = '2'
-  const span = 275
-  for (let i = 2; i <= 6; i++) {
-    str += ` ${span * i}:${i}`
-  }
-  return str
+const router = useRouter()
+const jumpTo = (path) => {
+  console.log(path)
+  router.push(path)
 }
 
+const moduleRouteStore = useModuleRouteStore()
+const { filterPattern } = storeToRefs(moduleRouteStore)
+const isShow = computed(() => {
+  return calIsShow(props.module, filterPattern.value)
+  // return props.module.path.indexOf(filterPattern.value) !== -1
+})
+
 </script>
+
 <template>
-  <template v-if="module.meta.isLeaf !== true">
-    <n-card :title="module.name">
-      <n-grid
-        :cols="respondCols()"
-        x-gap="24"
-        y-gap="24"
-        responsive="self">
-        <n-grid-item v-for="item of module.children" :key="item.path" :span="isShow(item)">
-          <ModuleItem :module="item" />
-        </n-grid-item>
-      </n-grid>
-    </n-card>
-  </template>
+  <!-- <n-thing v-show="isShow" ref="cardRef" class="module-tool-card"> -->
+  <n-thing ref="cardRef" class="module-tool-card">
+    <template #header>{{ module.name }}</template>
+    <!-- <n-ellipsis :style="{ 'max-width': cardContentWidth }" :line-clamp="4"> -->
+    <span class="module-tool-card-desc">{{ module.meta.info.description }}</span>
+    <!-- <template #tooltip>
+        <div :style="{ 'max-width': cardContentWidth }">
+          {{ module.meta.info.description }}
+        </div>
+      </template>
+    </n-ellipsis> -->
+    <template #header-extra>
+      <n-button
+        type="primary"
+        size="tiny"
+        round
+        @click="jumpTo(module.meta.routePath)">Go</n-button>
+    </template>
+  </n-thing>
 </template>
+
+<style scoped>
+.module-tool-card {
+  height: 137px;
+  border: 1px solid var(--border-color);
+  padding: 10px;
+}
+.module-tool-card-desc {
+  text-overflow: -o-ellipsis-lastline;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    line-clamp: 4;
+    -webkit-box-orient: vertical;
+}
+</style>
--- src/web/src/views/HomeView/index.vue
+++ src/web/src/views/HomeView/index.vue
@@ -1,22 +1,46 @@
 <script setup>
-import Module from './components/Module.vue'
 import { useModuleRouteStore } from '@/stores/moduleRoutes';
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+import Module from './components/Module.vue'
 
 const moduleRouteStore = useModuleRouteStore()
 
 const moduleRoute = moduleRouteStore.getFiltered()
 console.log(moduleRoute)
 const { filterPattern } = storeToRefs(moduleRouteStore)
+
+const isShow = computed(() => module => {
+  return (module.path.indexOf(filterPattern.value) !== -1) ? 1 : 0
+})
+
+const respondCols = () => {
+  let str = '2'
+  const span = 275
+  for (let i = 2; i <= 6; i++) {
+    str += ` ${span * i}:${i}`
+  }
+  return str
+}
+
 </script>
 
 <template>
   <div class="module-box">
-    {{ filterPattern }}
     <!-- modules   -->
     <n-grid :cols="1" y-gap="40">
-      <n-gi v-for="item of moduleRoute.children" :key="item.meta.routePath">
-        <Module :module="item" />
+      <n-gi v-for="moduleType of moduleRoute.children" :key="moduleType.meta.routePath">
+        <n-card :title="moduleType.name">
+          <n-grid
+            :cols="respondCols()"
+            x-gap="24"
+            y-gap="24"
+            responsive="self">
+            <n-grid-item v-for="item of moduleType.children" :key="item.path" :span="isShow(item)">
+              <Module :module="item" />
+            </n-grid-item>
+          </n-grid>
+        </n-card>
       </n-gi>
     </n-grid>
   </div>
