86a632259315ab7161af4fa338f74c6b814e3eb5
lynne00 <1010673763@qq.com>
2023-02-09 16:29:46 +0800

    时差计算器初步界面

--- src/web/src/modules/timecal.vue
+++ src/web/src/modules/timecal.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { ref, computed } from 'vue';
+import  moment from 'moment'
+import { onMounted} from 'vue'
+
 const starttime = ref(
   {
     startyear: 2044,
@@ -8,12 +11,6 @@ const starttime = ref(
     starthour: 0,
     startmin: 0,
     startsecond: 0,
-    styear: [],
-    stmon: [],
-    stday: [],
-    stmin: [],
-    sthour: [],
-    stsecond: []
   }
 )
 const endtime = ref(
@@ -24,83 +21,75 @@ const endtime = ref(
     endhour: 0,
     endmin: 0,
     endsecond: 0,
-    edyear: [],
-    edmon: [],
-    edday: [],
-    edhour: [],
-    edmin: [],
-    edsecond: []
   }
 )
 const scaleoption = ref(
   ['年', '月', '天', '小时', '分钟', '秒']
 )
+const time =ref(new Date()) 
+
+onMounted(() => {//⽣命周期模板挂载之后
+  this.timer = setInterval(() => {
+    document.getElementById("nowTime").innerHTML =  timestampToTime(new Date());
+  }, 1000)
+})
+
+function timestampToTime(timestamp) {
+    return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+}
+
 </script>
 
 <template>
-  <mx-row>
-    开始时间：
-    <div>
-      <n-input-group>
-        <select>
-          <option v-for="n in starttime.styear" :value=n>{{ n }}</option>
-        </select>年
-        <select>
-          <option v-for="n in starttime.stmon" :value=n>{{ n }}</option>
-        </select>月
-        <select>
-          <option v-for="n in starttime.stday" :value=n>{{ n }}</option>
-        </select>日
-        <select>
-          <option v-for="n in starttime.sthour" :value=n>{{ n }}</option>
-        </select>时
-        <select>
-          <option v-for="n in starttime.stmin" :value=n>{{ n }}</option>
-        </select>分
-        <select>
-          <option v-for="n in starttime.stsecond" :value=n>{{ n }}</option>
-        </select>秒
+  <mx-row class="horizontal">
+    <div class="title">当前时间：北京时间 <a id="nowTime">{{ timestampToTime(time)}}</a></div>
+  </mx-row>
+  <mx-row class="horizontal">
+    <div class="top-title">
+      <n-input-group class="mt-20">
+        开始时间：
+        <n-date-picker />
+        <n-time-picker />
       </n-input-group>
     </div>
   </mx-row>
-  <mx-row>
-    结束时间：
-    <div>
-      <n-input-group>
-        <select>
-          <option v-for="n in endtime.edyear" :value=n>{{ n }}</option>
-        </select>年
-        <select>
-          <option v-for="n in endtime.edmon" :value=n>{{ n }}</option>
-        </select>月
-        <select>
-          <option v-for="n in endtime.edday" :value=n>{{ n }}</option>
-        </select>日
-        <select>
-          <option v-for="n in endtime.edhour" :value=n>{{ n }}</option>
-        </select>时
-        <select>
-          <option v-for="n in endtime.edmin" :value=n>{{ n }}</option>
-        </select>分
-        <select>
-          <option v-for="n in endtime.edsecond" :value=n>{{ n }}</option>
-        </select>秒
+  <mx-row class="horizontal">
+    <div class="b-title">
+      <n-input-group class="mt-20">
+        结束时间：
+        <n-date-picker />
+        <n-time-picker />
       </n-input-group>
     </div>
   </mx-row>
-  <mx-row>
-    <n-button>计算时间差</n-button>
-    <n-input-group>
-      <div>
-        <n-input placeholder="时间差"></n-input>
-      </div>
-      <select>
-        <option v-for="n in scaleoption" :value=n>{{ n }}</option>
-      </select>
-    </n-input-group>
+  <mx-row class="horizontal">
+    <div class="mt-60">
+      <n-input-group class="mb-30">
+        <n-button color="#8a2be2" size="large" class="mr-20">计算时间差</n-button>
+        <div>
+          <n-input size="large" placeholder="时间差"></n-input>
+        </div>
+      </n-input-group>
+    </div>
   </mx-row>
 </template>
 
 <style scoped lang="scss">
+.title{
+  font-size: 35px;
+}
+.top-title {
+  font-size: large;
+  color: darkcyan;
+}
 
+.b-title {
+  font-size: large;
+  color: burlywood;
+}
+
+.horizontal {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
