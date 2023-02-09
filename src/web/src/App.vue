4a11cb335be710f93a577b99c6ef78a5b72e8060
毛线球上的喵星人 <785315332@qq.com>
2023-02-09 17:05:23 +0800

    更新时间差

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -1,19 +1,23 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import MxHeader from '@/components/MxHeader.vue'
+import { zhCN, dateZhCN  } from 'naive-ui'
 </script>
 
 <template>
-  <n-message-provider>
-    <n-layout class="wrapper">
-      <n-layout-header>
-        <mx-header />
-      </n-layout-header>
-      <n-layout-content content-style="padding: 20px 10% 0 10%;">
-        <RouterView />
-      </n-layout-content>
-    </n-layout>
-  </n-message-provider>
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+    <n-date-picker />
+    <n-message-provider>
+      <n-layout class="wrapper">
+        <n-layout-header>
+          <mx-header />
+        </n-layout-header>
+        <n-layout-content content-style="padding: 20px 10% 0 10%;">
+          <RouterView />
+        </n-layout-content>
+      </n-layout>
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <style scoped>
--- src/web/src/modules/timecal.vue
+++ src/web/src/modules/timecal.vue
@@ -1,55 +1,65 @@
 <script setup>
-import { ref, computed } from 'vue';
-import  moment from 'moment'
-import { onMounted} from 'vue'
+import { ref, computed, onMounted, onUnmounted } from 'vue';
+import moment from 'moment'
+let timer = 0
 
-const starttime = ref(
-  {
-    startyear: 2044,
-    startmon: 1,
-    startday: 1,
-    starthour: 0,
-    startmin: 0,
-    startsecond: 0,
-  }
-)
-const endtime = ref(
-  {
-    endyear: 2058,
-    endmon: 1,
-    endday: 1,
-    endhour: 0,
-    endmin: 0,
-    endsecond: 0,
-  }
-)
-const scaleoption = ref(
-  ['年', '月', '天', '小时', '分钟', '秒']
-)
-const time =ref(new Date()) 
+const nowTime = ref('')
 
-onMounted(() => {//⽣命周期模板挂载之后
-  this.timer = setInterval(() => {
-    document.getElementById("nowTime").innerHTML =  timestampToTime(new Date());
-  }, 1000)
+const time = ref({
+  start: null,
+  // 组件的时间用的是时间戳
+  end: moment().valueOf()
+})
+
+onMounted(() => {
+  timer = setInterval(() => {
+    nowTime.value = moment().format('YYYY-MM-DD HH:mm:ss')
+    // 100比较准
+  }, 100)
+})
+onUnmounted(() => {
+  timer && clearInterval(timer)
+})
+
+// 用computed自动计算，当 time.start || time.end这两个值变的时候会自动更新 timeResult
+const timeResult = computed(() => {
+  let duration = null
+  if (time.value.start && time.value.end) {
+    duration = moment.duration(time.value.end - time.value.start)
+  }
+  return duration ? {
+    // floor 向下取整
+    years: Math.floor(duration.asYears()),
+    months: Math.floor(duration.asMonths()),
+    weeks: Math.floor(duration.asWeeks()),
+    days: Math.floor(duration.asDays()),
+    hours: Math.floor(duration.asHours()),
+    minutes: Math.floor(duration.asMinutes()),
+  } : {
+    years: 0,
+    months: 0,
+    weeks: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+  }
 })
 
 function timestampToTime(timestamp) {
-    return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+  return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
 }
 
 </script>
 
 <template>
   <mx-row class="horizontal">
-    <div class="title">当前时间：北京时间 <a id="nowTime">{{ timestampToTime(time)}}</a></div>
+    <div class="title">当前时间：北京时间 <a id="nowTime">{{ nowTime }}</a></div>
   </mx-row>
   <mx-row class="horizontal">
     <div class="top-title">
       <n-input-group class="mt-20">
         开始时间：
-        <n-date-picker />
-        <n-time-picker />
+        <n-date-picker v-model:value="time.start" type="datetime" clearable />
       </n-input-group>
     </div>
   </mx-row>
@@ -57,27 +67,33 @@ function timestampToTime(timestamp) {
     <div class="b-title">
       <n-input-group class="mt-20">
         结束时间：
-        <n-date-picker />
-        <n-time-picker />
+        <n-date-picker v-model:value="time.end" type="datetime" clearable />
       </n-input-group>
     </div>
   </mx-row>
   <mx-row class="horizontal">
     <div class="mt-60">
-      <n-input-group class="mb-30">
-        <n-button color="#8a2be2" size="large" class="mr-20">计算时间差</n-button>
-        <div>
-          <n-input size="large" placeholder="时间差"></n-input>
-        </div>
-      </n-input-group>
+      <mx-row>
+        年： {{ timeResult.years }}
+      </mx-row>
+      <mx-row>
+        月：{{ timeResult.months }}
+      </mx-row>
+      <mx-row>
+        天：{{ timeResult.days }}
+      </mx-row>
+      <mx-row>
+        分钟: {{ timeResult.minutes }}
+      </mx-row>
     </div>
   </mx-row>
 </template>
 
 <style scoped lang="scss">
-.title{
+.title {
   font-size: 35px;
 }
+
 .top-title {
   font-size: large;
   color: darkcyan;
