47509de2bf7e22ad55293918f642a9d30d1d1b29
lynne00 <1010673763@qq.com>
2023-02-10 16:20:49 +0800

    时间差优化用户体验

--- src/web/src/modules/timecal.vue
+++ src/web/src/modules/timecal.vue
@@ -11,7 +11,7 @@ const time = ref({
   // 组件的时间用的是时间戳
   end: null,
   nowdata:moment().valueOf(),
-  dduration:1
+  dduration:0
 })
 
 onMounted(() => {
@@ -38,6 +38,12 @@ const timeResult = computed(() => {
     days: Math.floor(duration.asDays()),
     hours: Math.floor(duration.asHours()),
     minutes: Math.floor(duration.asMinutes()),
+    year: Math.floor(duration.years()),
+    month: Math.floor(duration.months()),
+    week:  Math.floor(duration.weeks()),
+    day: Math.floor(duration.days()),
+    hour: Math.floor(duration.hours()),
+    minute: Math.floor(duration.minutes()),
   } : {
     years: 0,
     months: 0,
@@ -45,6 +51,12 @@ const timeResult = computed(() => {
     days: 0,
     hours: 0,
     minutes: 0,
+    year: 0,
+    month: 0,
+    week: 0,
+    day: 0,
+    hour: 0,
+    minute: 0,
   }
 })
 
@@ -59,7 +71,7 @@ const timeResult = computed(() => {
           <div class="timecal-title">当前时间：北京时间 {{ nowTime }}</div>
         </mx-row>
         <mx-row class="timecal-horizontal">
-          <div class="timecal-top-title">
+          <div class="timecal-title">
             <n-input-group class="mt-20">
               开始时间：
               <n-date-picker v-model:value="time.start" type="datetime" clearable />
@@ -67,7 +79,7 @@ const timeResult = computed(() => {
           </div>
         </mx-row>
         <mx-row class="timecal-horizontal">
-          <div class="timecal-b-title">
+          <div class="timecal-title">
             <n-input-group class="mt-20">
               结束时间：
               <n-date-picker v-model:value="time.end" type="datetime" clearable />
@@ -79,16 +91,26 @@ const timeResult = computed(() => {
             <thead>
               <tr>
                 <th>时间单位</th>
-                <td>年</td>
-                <td>月</td>
-                <td>天</td>
-                <td>时</td>
-                <td>分</td>
+                <th>年</th>
+                <th>月</th>
+                <th>天</th>
+                <th>小时</th>
+                <th>分钟</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <th>时间差</th>
+                <th>两个时间差</th>
+                <td>{{ timeResult.year}}</td>
+                <td>{{ timeResult.month}}</td>
+                <td>{{ timeResult.day}}</td>
+                <td>{{ timeResult.hour}}</td>
+                <td>{{ timeResult.minute}}</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <th>按单位时间差</th>
                 <td>{{ timeResult.years }}</td>
                 <td>{{ timeResult.months }}</td>
                 <td>{{ timeResult.days }}</td>
@@ -99,12 +121,12 @@ const timeResult = computed(() => {
           </n-table>
         </n-space>
       </n-tab-pane>
-      <n-tab-pane name="计算x天后的日期">
+      <n-tab-pane name="计算距离x天的日期">
         <mx-row class="timecal-horizontal">
           <div class="timecal-title">当前时间：北京时间 {{ nowTime }}</div>
         </mx-row>
         <mx-row class="timecal-horizontal">
-          <div class="timecal-top-title">
+          <div class="timecal-title">
             <n-input-group class="mt-20">
               开始时间：
               <n-date-picker v-model:value="time.nowdata" type="date" clearable/>
@@ -112,16 +134,16 @@ const timeResult = computed(() => {
           </div>
         </mx-row>
         <mx-row class="timecal-horizontal">
-          <div class="timecal-top-title">
+          <div class="timecal-title">
             <n-input-group class="mt-20">
               距离天数：
-              <n-input-number v-model:value="time.dduration" placeholder="请输入整数" min="0" style="width: 200px"  />
+              <n-input-number v-model:value="time.dduration" placeholder="请输入整数" min="-99999999" max="9999999" style="width: 200px"  />
             </n-input-group>
           </div>
         </mx-row>
         <mx-row class="timecal-horizontal">
-          <div class="mt-30 timecal-down-title">
-            经过{{ time.dduration }}天后的日期为: {{moment().add(time.dduration,"days").format('YYYY-MM-DD') }}
+          <div class="mt-30 timecal-title">
+            {{ Math.abs(time.dduration) }} 天{{( time.dduration>0 ? '后':'前')}}的日期为: {{moment().add(time.dduration,"days").format('YYYY-MM-DD') }}
           </div>
         </mx-row>
       </n-tab-pane>
@@ -132,19 +154,8 @@ const timeResult = computed(() => {
 
 <style scoped lang="scss">
 .timecal-title {
-  font-size: 35px;
+  font-size: var(--n-title-font-size)
 }
-.timecal-down-title {
-  font-size: 25px;
-}
-.timecal-top-title {
-  font-size: large;
-}
-
-.timecal-b-title {
-  font-size: large;
-}
-
 .timecal-horizontal {
   display: flex;
   justify-content: space-around;
