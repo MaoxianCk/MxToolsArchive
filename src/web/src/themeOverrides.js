8364e4072f351d5cf2aae4878e61c01cab70dfa1
毛线球上的喵星人 <785315332@qq.com>
2023-02-18 17:45:59 +0800

    统一naive组件样式

---
+++ src/web/src/themeOverrides.js
@@ -0,0 +1,8 @@
+export const themeOverrides = {
+    common: {
+        primaryColor: '#ff5151',
+        primaryColorHover:'#fa6666',
+        primaryColorPressed:'#e13434',
+        primaryColorSuppl:'#ff8989',
+    }
+}
