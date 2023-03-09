1773f064e1957fa3ac1fea1cd5fa7c96f9963259
lynne00 <1010673763@qq.com>
2023-03-09 14:13:21 +0800

    今天吃啥模块计时器函数不同步问题

--- src/web/src/modules/eatwhat/index.vue
+++ src/web/src/modules/eatwhat/index.vue
@@ -1,34 +1,37 @@
 <script setup>
-import { ref, onUnmounted, onBeforeMount } from 'vue'
+import { ref } from 'vue'
 import foodTemplates from './food.json'
 
 const num = ref(0)
 const food = ref('神麽？')
 const eatingTimes = ref(['早餐', '午餐', '晚餐'])
+//按钮加载
+const loading=ref(false)
 
 const changEatingTime = () => {
     num.value = (num.value + 1) % 3
 }
+//文字转动效果
 const scrollAnimate = () => {
     food.value = foodTemplates[0]
     foodTemplates.push(foodTemplates[0]);
     foodTemplates.shift();
     console.log(foodTemplates)
     setTimeout(() => {
+        // console.log('o',foodTemplates)
+        clearInterval(timer)
         let num = Math.floor(Math.random() * foodTemplates.length)
         food.value = foodTemplates[num]
-        console.log('o',foodTemplates)
-        clearInterval(timer)
-        console.log('out',foodTemplates)
+        // console.log('out',foodTemplates)
+        loading.value=false
     }, 5000);
 }
-let timer = null
+let timer = 0
 const getScroll = () => {
+    loading.value=true
     timer = setInterval(scrollAnimate, 300)
 }
-onUnmounted(() => {
-    clearInterval(timer)
-})
+
 </script>
 
 <template>
@@ -43,7 +46,7 @@ onUnmounted(() => {
         </n-tooltip>
     </n-space>
     <n-space justify="center">
-        <n-button round color="#ff69b4" class="mt-80 mb-10" @click="getScroll()">
+        <n-button round color="#ff69b4" :loading="loading" class="mt-80 mb-10" @click="getScroll()">
             开始
         </n-button>
     </n-space>
